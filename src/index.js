c257dcdf8b8752996e4146d5d8421ef5e0fcfe47
Ian Harvey <ith.harvey@gmail.com>
2017-10-01 17:17:01 -0500

    completed front end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route  } from 'react-router-dom'
 import reduxThunk from 'redux-thunk'
+import { CookiesProvider } from 'react-cookie';
+import { withCookies, Cookies } from 'react-cookie';
 
 import Home from './components'
 import Mylibrary from './components/component_mylibrary'
@@ -12,25 +14,33 @@ import NotFoundPage from './components/component_notfound';
 
 import Register from './components/auth/component_register';
 import Login from './components/auth/component_login';
-import RequireAuth from './components/auth/component_require-auth';
+import RequireAuth from './components/auth/component_require_auth';
 
 
 import reducers from './reducers';
-import { AUTH_USER } from './actions/types';
+import { AUTH_USER } from './actions';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+console.log('cookie',cookie)
+const token = cookie.load('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <Switch>
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path='/mylibrary' component={Mylibrary}/>
-        <Route path='/' component={Home}/>
+  <CookiesProvider>
+    <BrowserRouter>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <Switch>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path='/mylibrary' component={Mylibrary}/>
+          <Route path='/' component={RequireAuth(Home)}/>
 
-        <Route path='/*' component={NotFoundPage}/>
-      </Switch>
-    </Provider>
-  </BrowserRouter>
+          <Route path='/*' component={NotFoundPage}/>
+        </Switch>
+      </Provider>
+    </BrowserRouter>
+  </CookiesProvider>
   , document.querySelector('.container'));
